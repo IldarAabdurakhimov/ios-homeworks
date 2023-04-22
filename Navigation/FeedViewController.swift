c02a1a53b5ada10ef3d5fe5f237f1a96eda691f5
IldarAbdurakhimov <rustaveliano@outlook.com>
2023-04-22 15:19:49 -0700

    added lazy property in FeedView

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -9,32 +9,33 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //   view.backgroundColor = .white // realizovano v Scene
-        view.backgroundColor = .white
-        
-        let button = UIButton()
-        button.setTitle("Перейти к посту", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(button)
-        
-        NSLayoutConstraint.activate([
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
-        
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-    }
+         
+        private lazy var button: UIButton = {
+                let button = UIButton()
+                button.setTitle("Перейти к посту", for: .normal)
+                button.setTitleColor(.systemBlue, for: .normal)
+                button.translatesAutoresizingMaskIntoConstraints = false
+                return button
+            }()
     
-    @objc func didTapButton() {
-        let postVC = PostViewController()
-        postVC.selectedPost = "Выбранный пост" // передаем выбранный пост в PostViewController
-        navigationController?.pushViewController(postVC, animated: true) // делаем переход на экран поста
-    }
-}
-// Do any additional setup after loading the view.
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                
+                view.addSubview(button)
+                
+                NSLayoutConstraint.activate([
+                    button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                    button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+                ])
+                
+               button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+            }
+            
+            @objc private func didTapButton() {
+              
+                let postVC = PostViewController()
+                postVC.selectedPost = "Выбранный пост" // передаем выбранный пост в PostViewController
+                navigationController?.pushViewController(postVC, animated: true) // делаем переход на экран поста
+            }
+        }
